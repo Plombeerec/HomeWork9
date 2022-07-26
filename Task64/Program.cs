a66c2adc1bf88c14eca3b1c79542401a3134b3fc
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-26 11:58:46 +0300

    Task64 done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значение N.
+ //Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+
+int FindNumbersOfDigit(int n)
+ {
+    if( n < 10 ) return 1;
+    else
+    return 1 + FindNumbersOfDigit(n/10);
+ }
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindNumbersOfDigit(number));
