bc3898505305d2c25816e8ffc5a1951f05f6eef2
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-26 11:57:32 +0300

    Task 66 is done

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+
+
+int SumOfDigits(int m, int n)
+{
+    if(m < n) return n+SumOfDigits(m, n-1);
+    else return m;
+}
+
+int MinNum = Convert.ToInt32(Console.ReadLine());
+int MaxNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfDigits(MinNum, MaxNum));
